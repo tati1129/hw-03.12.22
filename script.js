c2c42f36ddae7be6e137704950f2218b15186138
tati1129 <tatka.bogat@gmail.com>
2022-12-06 16:07:35 +0100

    add hw 03.12

---
+++ script.js
@@ -0,0 +1,22 @@
+// Необходимо реализовать интерфейс, с полем ввода (textarea). При обновлении страницы текст должен сохраняться в поле ввода. 
+// Реализуйте данное приложение через textarea и событие input.
+
+
+
+const text_input = document.querySelector('.text_input');
+const btn = document.querySelector('.btn_clear');
+
+
+text_input.value = localStorage.getItem('text')
+text_input.addEventListener('input', (event) => {
+    const value = event.target.value;
+    // console.log(event.target.value);
+    localStorage.setItem('text', value);
+    // console.log(event.target.name);
+})
+
+btn.addEventListener('click', ()=>{
+    text.value = '';
+    localStorage.setItem('text', '')
+})
+
