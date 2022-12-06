57732d44c770f037f8bef3b3c85745e57b6e666a
tati1129 <tatka.bogat@gmail.com>
2022-12-06 17:07:05 +0100

    changes

--- script.js
+++ script.js
@@ -7,16 +7,16 @@ const text_input = document.querySelector('.text_input');
 const btn = document.querySelector('.btn_clear');
 
 
-text_input.value = localStorage.getItem('text')
+text_input.value = localStorage.getItem('textIn');
 text_input.addEventListener('input', (event) => {
     const value = event.target.value;
     // console.log(event.target.value);
-    localStorage.setItem('text', value);
+    localStorage.setItem('textIn', value);
     // console.log(event.target.name);
 })
 
 btn.addEventListener('click', ()=>{
     text.value = '';
-    localStorage.setItem('text', '')
+    localStorage.setItem('textIn', '')
 })
 
